9d3b04b092f5709e56c8f48a1388cfd97c096eab
ckissane <cole.kissane@gmail.com>
2022-02-25 21:26:55 +0000

    Deploying to gh-pages from @ CM-Tech/regl-sph-water@180192dd8635985ec5cd4dda59eead6d559dab14 🚀

--- scripts/index.js
+++ scripts/index.js
@@ -44,6 +44,32 @@ document.addEventListener("touchend", (e) => {
   pointer.x = e.changedTouches[0].clientX;
   pointer.y = e.changedTouches[0].clientY;
 });
+function hslToRgb(h, s, l) {
+  var r, g, b;
+  if (s == 0) {
+    r = g = b = l;
+  } else {
+    var hue2rgb = function hue2rgb2(p2, q2, t) {
+      if (t < 0)
+        t += 1;
+      if (t > 1)
+        t -= 1;
+      if (t < 1 / 6)
+        return p2 + (q2 - p2) * 6 * t;
+      if (t < 1 / 2)
+        return q2;
+      if (t < 2 / 3)
+        return p2 + (q2 - p2) * (2 / 3 - t) * 6;
+      return p2;
+    };
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  return [r, g, b];
+}
 document.addEventListener("mousedown", () => {
-  pointer.color = [Math.random() + 0.2, Math.random() + 0.2, Math.random() + 0.2];
+  pointer.color = hslToRgb(Math.random(), 1, 0.5);
 });
